11005f65e2160a6e72eec95837cf72a287511ff4
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-04-23 00:18:56 +0530

    toaster add for notification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Admin from "./pages/Admin";
-import Layout from "./components/Layout"
-function App() {
-  
+import Layout from "./components/Layout";
 
+import { Toaster } from "react-hot-toast";
+function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/admin' element={<Admin/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/admin" element={<Admin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
